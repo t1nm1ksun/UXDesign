7bc12883729000785e68c1bb626c22dae1226b25
신민석 <minsuk07672@naver.com>
2023-11-22 15:43:37 +0900

    포기!

--- app/src/main/java/com/example/uxdesign/feature/favorite/FavoriteFragment.kt
+++ app/src/main/java/com/example/uxdesign/feature/favorite/FavoriteFragment.kt
@@ -1,11 +1,14 @@
 package com.example.uxdesign.feature.favorite
 
+import android.R
 import android.os.Bundle
+import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.uxdesign.databinding.FragmentFavoriteBinding
+
 
 class FavoriteFragment : Fragment() {
     private var _binding : FragmentFavoriteBinding? = null
@@ -25,6 +28,12 @@ class FavoriteFragment : Fragment() {
         /***
          * listener 달기
          */
+
+        /*var searchView: SearchView? = view.findViewById(R.id.search_view) as SearchView?
+        val id: Int = searchView.getContext().getResources()
+            .getIdentifier("android:id/search_src_text", null, null)
+        searchView.findViewById(id).setTextSize(TypedValue.COMPLEX_UNIT_SP, 16f) // 16sp*/
+
 
     }
 
